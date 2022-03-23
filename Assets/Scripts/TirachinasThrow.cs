69becc628ed46330a4d1cbb5960027d3a82cbb60
Jacinto Mba Cantero <jacmba@gmail.com>
2022-03-23 16:48:05 +0000

    Implemented launch control

--- Assets/Scripts/TirachinasThrow.cs
+++ Assets/Scripts/TirachinasThrow.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class TirachinasThrow : MonoBehaviour
 {
+  private enum State
+  {
+    IDLE,
+    DRAGGING,
+    RELEASED
+  }
+
   [SerializeField]
   private Transform[] positions;
 
   [SerializeField]
   private LineRenderer[] lines;
 
-  public Transform testObj;
+  [SerializeField]
+  private Transform pek;
+
+  [SerializeField]
+  private float launchForce = 500f;
+
+  [SerializeField]
+  private float upForce = 200f;
+
+  private bool mouseDown;
+  private State state;
+  Rigidbody pekBody;
+  private Vector3 origin;
 
   // Start is called before the first frame update
   void Start()
@@ -19,26 +38,94 @@ public class TirachinasThrow : MonoBehaviour
     {
       lines[i].SetPosition(0, positions[i].position);
     }
+
+    state = State.IDLE;
+    mouseDown = false;
+    pekBody = pek.GetComponent<Rigidbody>();
+    origin = pek.position;
   }
 
   // Update is called once per frame
   void Update()
   {
-    Vector3 mousePos = Input.mousePosition;
-    mousePos.z = Camera.main.nearClipPlane;
-    Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
-    Ray ray = Camera.main.ScreenPointToRay(mousePos);
-    RaycastHit hit;
-    if (Physics.Raycast(ray, out hit))
+    if (Input.GetMouseButtonDown(0))
     {
-      worldPosition = hit.point;
-      worldPosition.z = 0;
-      Debug.Log(worldPosition);
-      testObj.position = new Vector3(worldPosition.x, worldPosition.y, 0);
-      for (int i = 0; i < lines.Length; i++)
+      OnMouseDown();
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+      OnMouseUp();
+    }
+
+    if (mouseDown)
+    {
+      Vector3 mousePos = Input.mousePosition;
+      mousePos.z = Camera.main.nearClipPlane;
+      Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+      Ray ray = Camera.main.ScreenPointToRay(mousePos);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit, 10))
       {
-        lines[i].SetPosition(1, worldPosition);
+        if (state == State.DRAGGING)
+        {
+          worldPosition = hit.point;
+          worldPosition.z = 0;
+          worldPosition = origin + Vector3.ClampMagnitude(worldPosition - origin, 2f);
+          pek.position = new Vector3(worldPosition.x, worldPosition.y, 0);
+          pek.LookAt(origin, Vector3.forward * 5f);
+          for (int i = 0; i < lines.Length; i++)
+          {
+            lines[i].SetPosition(1, worldPosition);
+          }
+        }
       }
+    }
+    else
+    {
+      if (state == State.IDLE)
+      {
+        foreach (LineRenderer line in lines)
+        {
+          line.SetPosition(1, pek.position);
+        }
+      }
+      else
+      {
+        foreach (LineRenderer line in lines)
+        {
+          Vector3 currentPos = line.GetPosition(1);
+          Vector3 newPos = Vector3.Lerp(currentPos, origin, 10f * Time.deltaTime);
+          line.SetPosition(1, newPos);
+        }
+      }
+    }
+  }
+
+  /// <summary>
+  /// OnMouseDown is called when the user has pressed the mouse button while
+  /// over the GUIElement or Collider.
+  /// </summary>
+  void OnMouseDown()
+  {
+    mouseDown = true;
+    if (state == State.IDLE)
+    {
+      state = State.DRAGGING;
+      pekBody.isKinematic = true;
+    }
+  }
+
+  /// <summary>
+  /// OnMouseUp is called when the user has released the mouse button.
+  /// </summary>
+  void OnMouseUp()
+  {
+    mouseDown = false;
+    if (state == State.DRAGGING)
+    {
+      state = State.RELEASED;
+      pekBody.isKinematic = false;
+      pekBody.AddForce(((origin - pek.position) * launchForce) + (Vector3.up * upForce), ForceMode.Acceleration);
     }
   }
 }

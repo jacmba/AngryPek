0860b78ed2b6be6305fd4c364aeccee6b2aa211a
Jacinto Mba Cantero <jacmba@gmail.com>
2022-04-29 12:25:21 +0100

    Play button on title screen

--- Assets/Scripts/Game/TitleController.cs
+++ Assets/Scripts/Game/TitleController.cs
@@ -6,34 +6,10 @@ using UnityEngine.EventSystems;
 
 public class TitleController : MonoBehaviour
 {
-  private bool canTouch;
 
-  /// <summary>
-  /// Start is called on the frame when a script is enabled just before
-  /// any of the Update methods is called the first time.
-  /// </summary>
-  void Start()
+  public void OnPlayClick()
   {
-    canTouch = false;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (EventSystem.current.currentSelectedGameObject != null)
-    {
-      return;
-    }
-
-    if (Input.GetMouseButtonDown(0) && canTouch)
-    {
-      SceneManager.LoadScene(1);
-    }
-
-    if (!Input.GetMouseButton(0))
-    {
-      canTouch = true;
-    }
+    SceneManager.LoadScene(1);
   }
 
   public void OnExitClick()
